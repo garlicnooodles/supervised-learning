8c165fa8dda7a57f31b268fc7fc365f8219d83a5
garlicnooodles <rubberbandshurt@gmail.com>
2023-05-30 00:12:01 +0530

    30052023

--- model_bin/linear_regression.py
+++ model_bin/linear_regression.py
@@ -17,15 +17,15 @@ def to_numpy_arrays(X_pd,y_pd):
 
 #calculating hypothesis
 def hyp(X_hyp,theta_hyp,b_hyp):
-    h=np.dot(X_hyp,theta_hyp)+b_hyp
-    return h
+    m_hyp,n_hyp = X_hyp.shape
+    h=np.dot(X_hyp, theta_hyp)+b_hyp
+    return h.reshape((m_hyp,1))
 
 # calculating the cost function
 def cost_function(X_cf,y_cf,theta_cf,b_cf):
     m_cf,n_cf = X_cf.shape
-    tv1=(y_cf-hyp(X_cf,theta_cf,b_cf))**2
-    tv2=np.sum(tv1)
-    J=tv2/(2*m_cf)
+    h=hyp(X_cf,theta_cf,b_cf)
+    J=np.sum(np.power((h-y_cf),2))/(2*m_cf)
     return J
 
 # computing the gradient for theta and b
@@ -33,28 +33,26 @@ def compute_gradient(X_gr,y_gr,theta_gr,b_gr):
     m_gr,n_gr = X_gr.shape
     dj_dw=np.zeros((n_gr,))
     dj_db=0
-    for i in range(m_gr):
-        err=(np.dot(X_gr[i],theta_gr)+b_gr)-y_gr[i]
-        for j in range(n_gr):
-            dj_dw[j]=(dj_dw[j]+err) * X_gr[i,j]
-        dj_db=(dj_db+err)
-    dj_dw=dj_dw/m_gr
-    dj_db=dj_db/m_gr
+    h=hyp(X_gr,theta_gr,b_gr)
+    err=h-y_gr
+    dj_dw=np.dot(X_gr.transpose(),err)/m_gr
+    dj_db=np.sum(err,axis=0)/m_gr
     return dj_dw,dj_db
 
 #gradient descent computation
 def gradient_descent(X_gd,y_gd,theta_in_gd,b_in_gd,alpha_gd,epoch_gd):
     X_gd,y_gd=to_numpy_arrays(X_gd,y_gd)
+    m_gd,n_gd = X_gd.shape
     plot_cost=[]
-    theta=theta_in_gd
+    theta=theta_in_gd.reshape((n_gd,1))
     b=b_in_gd
     for i in range(epoch_gd):
         dj_dw,dj_db=compute_gradient(X_gd,y_gd,theta,b)
-        theta=theta-alpha_gd*dj_dw
-        b=b-alpha_gd*dj_db
+        theta=theta-(alpha_gd*dj_dw)
+        b=b-(alpha_gd*dj_db)
         plot_cost.append(cost_function(X_gd,y_gd,theta,b))
         if i%(epoch_gd/10)==0:
-            print(f"iteration {i:4d}: Cost {plot_cost[-1]:8.2f} ")
+            print(f"iteration {i:4d}: Cost {plot_cost[-1]:8.5f} ")
     return theta,b,plot_cost
 
 #validation scores
